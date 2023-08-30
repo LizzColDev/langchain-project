06f097e08758fd3557e5028e7d1c02818fb507f0
lizzcoldev <lizzcoldev@gmail.com>
2023-08-29 23:34:13 -0500

    Install langchain

--- main.py
+++ main.py
@@ -1,8 +1,16 @@
 from fastapi import FastAPI
+from langchain.llms import LlamaCpp, OpenAI
+import config
+
+api = config.OPEN_API_KEY
 
 app = FastAPI()
+print(api)
+llm_openai = OpenAI(model_name = "text-davinci-003", openai_api_key=api)
 
+answer_openai = llm_openai("Hi, How are you?")
 
+print(answer_openai)
 @app.get("/")
 def read_root():
     return {"message": "Hello, World"}
